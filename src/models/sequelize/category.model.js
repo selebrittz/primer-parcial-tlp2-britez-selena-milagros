43229e98312b05d563683e695b96624ccc04d618
Selena Britez <selenabritez934@gmail.com>
2025-09-29 10:40:21 -0300

    relaciones con sequelize terminadas

--- src/models/sequelize/category.model.js
+++ src/models/sequelize/category.model.js
@@ -1,6 +1,8 @@
 import { DataTypes } from "sequelize";
 
-export const CategoryModel = sequelize.define("Category", {
-  name: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+export const CategoryModel = sequelize.define(
+  "Category", {
+  name: {
+  type: DataTypes.STRING(100), allowNull: false, unique: true },
   description: { type: DataTypes.TEXT, allowNull: true },
 });

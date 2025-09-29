d22135b15bd735178143a4121d8fff2867ad7022
Selena Britez <selenabritez934@gmail.com>
2025-09-29 10:22:15 -0300

    he creado la relacion embebida

--- src/models/mongoose/user.model.js
+++ src/models/mongoose/user.model.js
@@ -19,7 +19,27 @@ const UserSchema = new Schema(
       default: "secretary",
     },
     deletedAt: { type: Date, default: null },
-    // ! FALTA COMPLETAR ACA
+    
+    profile: {
+      employee_number: {
+      type: String,
+      unique: true,
+      required: true,
+      },
+      first_name: { 
+        type: String, 
+        required: true, 
+        minLength: 2, 
+        maxLength: 50 },
+      last_name: {
+         type: String,
+          required: true,
+          minLength: 2,
+          maxLength: 50 },
+      phone: {
+        type: String,
+      },
+    },
   },
   { timestamps: true }
 );
